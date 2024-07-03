0fc622ecc178e14c39ebb964531e41262ee977e6
vKenjoo <blasingkennviray@gmail.com>
2024-07-03 16:05:38 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<MainLayout />}>
+			<Route index element={<HomePage />} />
+			<Route path='/jobs' element={<JobsPage />} />
+		</Route>
+	)
+);
+
+const App = () => {
+	return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const JobsPage = () => {
+  return (
+    <div>JobPages</div>
+  )
+}
+
+export default JobsPage
