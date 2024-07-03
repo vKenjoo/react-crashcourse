2859d01877cbd0cff05fcfc20f989e51aa992ab7
vKenjoo <blasingkennviray@gmail.com>
2024-07-04 00:17:25 +0800

    continuation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import HomeCards from '../components/HomeCards'
-import JobListing from '../components/JobListing'
+import JobListings from '../components/JobListings'
 import ViewAllJobs from '../components/ViewAllJobs'
 
 const HomePage = () => {
@@ -9,7 +9,7 @@ const HomePage = () => {
     <>
       <Hero />
       <HomeCards />
-      <JobListing />
+      <JobListings />
       <ViewAllJobs />
     </>
   )
